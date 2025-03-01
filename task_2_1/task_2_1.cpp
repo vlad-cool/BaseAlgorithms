6b5f24fd84a954a9c7eab53b0d8d79567b3c2f53
vlad-cool <vlad-matveev2004@mail.ru>
2025-03-01 18:20:25 +0300

    plots to task 2.1 added

--- task_2_1/task_2_1.cpp
+++ task_2_1/task_2_1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <chrono>
+#include <string>
+#include <fstream>
 
 template <typename T>
 void swap(T *a, T *b)
@@ -10,7 +12,7 @@ void swap(T *a, T *b)
 }
 
 template <typename T>
-void copy(const T* src, T* dest, size_t size)
+void copy(const T *src, T *dest, size_t size)
 {
     for (size_t i = 0; i < size; i++)
     {
@@ -32,7 +34,7 @@ bool check_sorted(T *array, size_t size)
 }
 
 template <typename T>
-void array_bubble_sort(T *array, size_t size, bool with_check=false)
+void array_bubble_sort(T *array, size_t size, bool with_check = false)
 {
     bool sorted;
     for (size_t i = 1; i < size; i++)
@@ -54,7 +56,7 @@ void array_bubble_sort(T *array, size_t size, bool with_check=false)
 }
 
 template <typename T>
-void array_shaker_sort(T *array, size_t size, bool with_check=false)
+void array_shaker_sort(T *array, size_t size, bool with_check = false)
 {
     bool sorted;
     for (size_t i = 1; i < size / 2; i++)
@@ -85,57 +87,83 @@ void array_shaker_sort(T *array, size_t size, bool with_check=false)
     }
 }
 
-int main()
+#define COMB_COEFFICIENT 2 / 3
+template <typename T>
+void array_comb_sort(T *array, size_t size, bool with_check=false)
 {
-    int *source_array, *array;
+    bool sorted = false;
+    size_t distance = size;
+
+    while (!sorted || distance > 1)
+    {
+        distance = distance * COMB_COEFFICIENT;
+        if (distance < 1)
+        {
+            distance = 1;
+        }
+
+        sorted = true;
+        for (size_t i = 0; i + distance < size; i++)
+        {
+            if (array[i] > array[i + distance])
+            {
+                sorted = false;
+                swap(array + i, array + i + distance);
+            }
+        }
+    }
+}
+template <typename T>
+using SortingFunction = void (*)(T*, size_t, bool);
+
+template <typename T>
+void test_sort(T *source_array, size_t size, SortingFunction<T> sorting_function, std::string sorting_name, std::ofstream &plots_data)
+{
+    T *array = new T[size];
+
+    for (int with_checking = 0; with_checking < 2; with_checking++)
+    {
+        copy(source_array, array, size);
+        auto start = std::chrono::high_resolution_clock::now();
+        array_comb_sort(array, size, with_checking);
+        auto end = std::chrono::high_resolution_clock::now();
+        if (check_sorted(array, size))
+        {
+            std::cout << "\033[0;32mOK\033[0;0m " << sorting_name << (with_checking ? " with checking" : "") << " successfully sorted array of " << size << " elements. It took " << (end - start).count() * 1e-9 << " seconds" << std::endl;
+            plots_data << sorting_name << (with_checking ? " with check" : "") << ", " << size << ", " << (end - start).count() * 1e-9 << "\n";
+        }
+        else
+        {
+            std::cout << "\033[1;31mERROR\033[0;0m " << sorting_name << (with_checking ? " with checking" : "") << " failed to sort array of " << size << "elements" << std::endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    int *source_array;
     size_t size;
-    
+
     std::cin >> size;
 
     source_array = new int[size];
-    array = new int[size];
 
     for (size_t i = 0; i < size; i++)
     {
         std::cin >> source_array[i];
     }
 
-    copy(source_array, array, size);
-    
-    auto start = std::chrono::high_resolution_clock::now();
-    array_bubble_sort(array, size);
-    auto end = std::chrono::high_resolution_clock::now();
-    // for (size_t i = 0; i < size; i++)
-    // {
-    //     std::cout << array[i] << " ";
-    // }
-    std::cout << std::endl;
-    if (check_sorted(array, size))
+    std::string plots_data_path = "/dev/null";
+    if (argc > 1)
     {
-        std::cout << "\033[0;32mOK\033[0;0m" << std::endl;
-        std::cout << "Buble sort successfully sorted array of " << size << " elements. It took " << (end - start).count() * 1e-9 << " seconds" << std::endl;
-    }
-    else
-    {
-        std::cout << "\033[1;31mERROR\033[0;0m" << std::endl;
+        plots_data_path = argv[1];
     }
 
-    copy(source_array, array, size);
-    array_shaker_sort(array, size);
-    // for (size_t i = 0; i < size; i++)
-    // {
-    //     std::cout << array[i] << " ";
-    // }
-    std::cout << std::endl;
-    if (check_sorted(array, size))
-    {
-        std::cout << "\033[0;32mOK\033[0;0m" << std::endl;
-    }
-    else
-    {
-        std::cout << "\033[1;31mERROR\033[0;0m" << std::endl;
-    }
+    std::ofstream outfile(plots_data_path, std::ios::app);
 
+    test_sort(source_array, size, array_bubble_sort, "Bubble sort", outfile);
+    test_sort(source_array, size, array_shaker_sort, "Shaker sort", outfile);
+    test_sort(source_array, size, array_comb_sort, "Comb sort", outfile);
 
-    delete[] array;
+    delete[] source_array;
 }
--- task_2_1/test.py
+++ task_2_1/test.py
@@ -3,6 +3,8 @@ import subprocess
 import os
 import random
 
+subprocess.run(["rm", os.path.join(os.path.dirname(__file__), "plots_data.csv")])
+
 with open(os.path.join(os.path.dirname(__file__), "tests.txt"), "r") as f:    
     for line in f.readlines():
         size, min_val, max_val = list(map(int, line.split()))
@@ -10,11 +12,10 @@ with open(os.path.join(os.path.dirname(__file__), "tests.txt"), "r") as f:
         for j in range(size):
             a.append(random.randrange(min_val, max_val, 5))
         
-        result = subprocess.run([os.path.join(os.path.dirname(__file__), "./task_2_1")],
+        result = subprocess.run([os.path.join(os.path.dirname(__file__), "./task_2_1"), os.path.join(os.path.dirname(__file__), "plots_data.csv")],
                                 input=" ".join(list(map(str, a))),
                                 capture_output=True,
                                 text=True)
 
         print(result.stdout.strip())
-        
-        # print("\033[0;32mOK\033[0;0m" if result.stdout.strip() == outputs[i].strip() else f"\033[1;31mERROR on test {i}\033[0;0m")
+    
