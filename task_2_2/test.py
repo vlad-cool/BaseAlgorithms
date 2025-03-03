4cc5222c6366178f37775c5d792c8088d20e817d
vlad-cool <vlad-matveev2004@mail.ru>
2025-03-03 14:29:36 +0300

    sort testing fixed

--- task_2_2/test.py
+++ task_2_2/test.py
@@ -45,12 +45,9 @@ with open(os.path.join(os.path.dirname(__file__), "tests_shuffle.txt"), "r") as 
         a.sort()
         
         for i in range(0, size * shuffle_val // 100):
-            a[random.randint(1, size - 1)] = random.randint(min_val, max_val)
+            a[random.randint(0, size - 1)] = random.randint(min_val, max_val)
         
         a = [size, *a]
-        
-        for i in range(0, size * shuffle_val // 100):
-            a[random.randint(0, size - 1)] = random.randint(min_val, max_val)
         
         result = subprocess.run([os.path.join(os.path.dirname(__file__), "./task_2_2"), os.path.join(os.path.dirname(__file__), "plots_data_shuffle.csv"), str(shuffle_val)],
                                 input=" ".join(list(map(str, a))),
